ea6eb7a328278a3d8b129e6541b26a0370e2619e
Hazem Khaled <hazem.khaled1293@gmail.com>
2022-07-14 22:24:12 +0200

    fix for the user avatar

--- FrontEnd/src/components/user-avatar.tsx
+++ FrontEnd/src/components/user-avatar.tsx
@@ -1,17 +1,18 @@
-
-import { UserDTO } from 'api-models'
-
 interface HeaderProps {
-	user?: UserDTO | null,
-	size?: number
-	src?: string
+  user?:any;
+  size?: number;
+  src?: string;
 }
 
 export const UserAvatar = ({ user, size, src }: HeaderProps) => (
-
 	<img
-		className={`inline-block h-${size || 10} w-${size || 10} rounded-full border-2 border-gray-300`}
-		src={src || user?.avatar || `https://eu.ui-avatars.com/api/?name=${user?.name}`} alt='user photo'
+		className={`inline-block h-${size || 10} w-${
+			size || 10
+		} rounded-full border-2 border-gray-300`}
+		src={
+			src || user?.avatar || `https://eu.ui-avatars.com/api/?name=${user?.name}`
+		}
+		alt="user photo"
 	/>
 
 	// <div className="flex-shrink-0">
