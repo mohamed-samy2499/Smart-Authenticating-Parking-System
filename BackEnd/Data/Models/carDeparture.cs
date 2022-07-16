c3959c876e024f2527557493bb7893c67207d914
mohamed-samy2499 <1701198@eng.asu.edu.eg>
2022-07-16 13:45:33 +0200

    user left or entered added

---
+++ BackEnd/Data/Models/carDeparture.cs
@@ -0,0 +1,10 @@
+﻿namespace Parking_System_API.Data.Models
+{
+    public class carDeparture
+    {
+
+        public string faceId { get; set; }
+        public string plateId { get; set; }
+
+    }
+}
