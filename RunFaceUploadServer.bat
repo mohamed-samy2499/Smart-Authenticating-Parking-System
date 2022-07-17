034f9291a2b4885addf4bfbac0fe148201f6faac
mohamed-samy2499 <1701198@eng.asu.edu.eg>
2022-07-17 21:42:17 +0200

    adding patch files for the 3 servers

--- RunFaceUploadServer.bat
+++ RunFaceUploadServer.bat
@@ -1,6 +1,1 @@
-set /p in_dir="Enter input directory: "
-set /p out_dir="Enter output directory: "
-set /p choice="Enter 0 for running on image or 1 for running on video: "
-set /p debug="Enter 1 for debug mode on image or 0 for none: "
-
-python main.py %in_dir% %out_dir% %choice% %debug%
+cd FaceModule && python detectFaceApi.py
